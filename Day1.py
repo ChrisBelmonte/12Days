a2a40282c1aab43790e851b05a46824ceab4cf0b
Christopher Belmonte <christopherbelmo@yahoo.com>
2015-12-16 12:16:41 -0500

    finished day 1!

--- Day1.py
+++ Day1.py
@@ -1,14 +1,11 @@
 i = 0
 level = 0
 
-while i == 0:
-    print "You are on floor " + str(level) + "."
-    chooseFloor = raw_input()
-    if chooseFloor == 'Done' or 'done':
-        i += 1
-    else:
-        for character in chooseFloor:
-            if character == '(':
-                level += 1
-            elif character == ")":
-                level -= 1
+with open('Day1Input.txt') as file:
+        for line in file:
+            for character in line:
+                if character == '(':
+                    level += 1
+                elif character == ')':
+                    level -= 1
+print level
